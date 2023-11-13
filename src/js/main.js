efdb2e0ce2f4a8049785504b2380136c1e55a9d9
giovannipedro <40199110@roctilburg.nl>
2023-11-13 12:09:15 +0100

    navbar

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,21 @@ $(document).ready(function () {
   var navbar = $("<nav>");
   navbar.html(
     '<div class="footer-menu">\
+<<<<<<< Updated upstream
           <ul class="side-categories">\
             <li><a href="../home/">Home</a></li>\
             <li><a href="../about/">About</a></li>\
             <li><a href="../pakketen/">Products</a></li>\
           </ul>\
         </div>'
+=======
+            <ul class="side-categories">\
+              <li><a href="../home/"><img src="src/img/icons/Bookings.svg" alt="bookings"></a></li>\
+              <li><a href="../about/"><img src="src/img/icons/home.svg" alt="home"></a></li>\
+              <li><a href="../pakketen/"><img src="src/img/icons/profile.svg" alt="profile"></a></li>\
+            </ul>\
+          </div>'
+>>>>>>> Stashed changes
   );
   $("body").append(navbar);
 });
